0df0a52e75110688cc6fb13e392eb34f9b9c1719
AlexanderBoss <alexanderpmiranda1996@gmail.com>
2022-02-27 23:11:41 -0500

    Commit inicial

---
+++ public/js/firebase.js
@@ -0,0 +1,15 @@
+
+let firebaseConfig = {
+    apiKey: "AIzaSyAIjDoN7PODcgjr61izQ8xiEYkQY12A9ZE",
+    authDomain: "blogging-website-7b55d.firebaseapp.com",
+    projectId: "blogging-website-7b55d",
+    storageBucket: "blogging-website-7b55d.appspot.com",
+    messagingSenderId: "362915573716",
+    appId: "1:362915573716:web:e38f0fcd4bcaf89f47b041"
+  };
+
+ // Initialize Firebase
+
+ firebase.initializeApp(firebaseConfig);
+
+ let db = firebase.firestore();
